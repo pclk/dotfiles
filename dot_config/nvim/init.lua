0c22924328c13afb5a972d2249cd346815d6737b
pclk <siiewweiheng@gmail.com>
2024-09-04 09:54:58 +0800

    feat: better nvim tokyonight transparency

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,9 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-function Transparent(color)
-  color = color or "tokyonight-night"
-  vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-Transparent()
+-- function Transparent(color)
+--   color = color or "tokyonight-night"
+--   vim.cmd.colorscheme(color)
+--   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- end
+-- Transparent()
